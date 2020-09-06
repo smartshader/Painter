c43600d6553dc66e9e69a2c04d1e8887736c10ab
Imad Jerra <smartshader@gmail.com>
2020-09-06 12:56:39 -0400

    Rotate Cannon

--- Painter.cs
+++ Painter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
@@ -12,8 +13,12 @@ namespace Painter
 
         private Texture2D _background;
         private Texture2D _balloon;
+        private Texture2D _cannonBarrel;
 
         private Vector2 _balloonPosition, _balloonOrigin;
+        private Vector2 _cannonBarrelPosition, _cannonBarrelOrigin;
+
+        private float _barrelAngle;
 
         public Painter()
         {
@@ -36,6 +41,7 @@ namespace Painter
             // TODO: use this.Content to load your game content here
             _balloon = Content.Load<Texture2D>("spr_lives");
             _background = Content.Load<Texture2D>("spr_background");
+            _cannonBarrel = Content.Load<Texture2D>("spr_cannon_barrel");
             MediaPlayer.Play(Content.Load<Song>("snd_music"));
         }
 
@@ -46,10 +52,14 @@ namespace Painter
 
             // TODO: Add your update logic here
             MouseState currentMouseState = Mouse.GetState();
-            // _balloonPosition = new Vector2(currentMouseState.X - _balloon.Width / 2,
-            //     currentMouseState.Y - _balloon.Height);
             _balloonOrigin = new Vector2(_balloon.Width / 2f, _balloon.Height);
+            _cannonBarrelOrigin = new Vector2(_cannonBarrel.Height, _cannonBarrel.Height) / 2;
+            
             _balloonPosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+            _cannonBarrelPosition = new Vector2(72, 405);
+
+            _barrelAngle = (float) Math.Atan2(currentMouseState.Y - _cannonBarrelPosition.Y,
+                currentMouseState.X - _cannonBarrelPosition.X);
 
             base.Update(gameTime);
         }
@@ -62,6 +72,8 @@ namespace Painter
             _spriteBatch.Begin();
             _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
             _spriteBatch.Draw(_balloon, _balloonPosition - _balloonOrigin, Color.White);
+            _spriteBatch.Draw(_cannonBarrel, _cannonBarrelPosition, null, Color.White,
+                _barrelAngle, _cannonBarrelOrigin, 1.0f, SpriteEffects.None, 0);
             _spriteBatch.End();
 
             base.Draw(gameTime);
