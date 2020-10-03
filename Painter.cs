269edbf2852f215e5c589cc368340308126cc8a5
Imad Jerra <smartshader@gmail.com>
2020-10-03 17:53:01 -0400

    Exercise 5.6.2.a

--- Painter.cs
+++ Painter.cs
@@ -15,12 +15,12 @@ namespace Painter
 
         private Texture2D _background;
         private Texture2D _balloon;
-        private Texture2D _balloon1;
-        private Texture2D _balloon2;
-        private Texture2D _balloon3;
-        private Vector2 _balloon1Position;
-        private Vector2 _balloon2Position;
-        private Vector2 _balloon3Position;
+        private Texture2D _balloonMiddleScreen;
+        private Texture2D _balloonTopRight;
+        private Texture2D _balloonBottomLeft;
+        private Vector2 _balloonMiddleScreenPosition;
+        private Vector2 _balloonTopRightPosition;
+        private Vector2 _balloonBottomLeftPosition;
         private Texture2D _cannonBarrel;
 
         private Vector2 _balloonPosition, _balloonOrigin;
@@ -48,9 +48,9 @@ namespace Painter
 
             // TODO: use this.Content to load your game content here
             _balloon = Content.Load<Texture2D>("spr_lives");
-            _balloon1 = Content.Load<Texture2D>("spr_lives");
-            _balloon2 = Content.Load<Texture2D>("spr_lives");
-            _balloon3 = Content.Load<Texture2D>("spr_lives");
+            _balloonMiddleScreen = Content.Load<Texture2D>("spr_lives");
+            _balloonTopRight = Content.Load<Texture2D>("spr_lives");
+            _balloonBottomLeft = Content.Load<Texture2D>("spr_lives");
             _background = Content.Load<Texture2D>("spr_background");
             _cannonBarrel = Content.Load<Texture2D>("spr_cannon_barrel");
             MediaPlayer.Play(Content.Load<Song>("snd_music"));
@@ -74,9 +74,11 @@ namespace Painter
             _barrelAngle = (float) Math.Atan2(currentMouseState.Y - _cannonBarrelPosition.Y,
                 currentMouseState.X - _cannonBarrelPosition.X);
             
-            _balloon1Position = new Vector2(_windowWidth / 2f, _windowHeigth / 2f);
-            _balloon2Position = new Vector2(_windowWidth - _balloon2.Width, 0);
-            _balloon3Position = new Vector2(0, _windowHeigth - _balloon3.Height);
+            _balloonMiddleScreenPosition = new Vector2(_windowWidth / 2f, _windowHeigth / 2f);
+            _balloonTopRightPosition = new Vector2(
+                _windowWidth - _balloonTopRight.Width, gameTime.TotalGameTime.Milliseconds / 1000f * _windowHeigth);
+            _balloonBottomLeftPosition = new Vector2(
+                gameTime.TotalGameTime.Milliseconds / 1000f * _windowWidth, _windowHeigth - _balloonBottomLeft.Height);
 
             base.Update(gameTime);
         }
@@ -89,9 +91,9 @@ namespace Painter
             _spriteBatch.Begin();
             _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
             _spriteBatch.Draw(_balloon, _balloonPosition - _balloonOrigin, Color.White);
-            _spriteBatch.Draw(_balloon1, _balloon1Position, Color.Red);
-            _spriteBatch.Draw(_balloon2, _balloon2Position, Color.Red);
-            _spriteBatch.Draw(_balloon3, _balloon3Position, Color.Red);
+            _spriteBatch.Draw(_balloonMiddleScreen, _balloonMiddleScreenPosition, Color.Red);
+            _spriteBatch.Draw(_balloonTopRight, _balloonTopRightPosition, Color.Red);
+            _spriteBatch.Draw(_balloonBottomLeft, _balloonBottomLeftPosition, Color.Red);
             _spriteBatch.Draw(_cannonBarrel, _cannonBarrelPosition, null, Color.White,
                 _barrelAngle, _cannonBarrelOrigin, 1.0f, SpriteEffects.None, 0);
             _spriteBatch.End();
